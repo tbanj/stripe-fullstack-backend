6a83a1f08e02b7de81e5939a07a0e803dd3edd6f
MAC <mac@MACs-MacBook-Pro.local>
2025-03-18 20:13:49 +0100

    updated

--- src/config/database.js
+++
@@ -1,8 +0,0 @@
-const { neon } = require("@neondatabase/serverless");
-require("dotenv").config();
-
-const databaseUrl = process.env.DATABASE_URL;
-
-const sql = neon(databaseUrl);
-
-module.exports = sql;
